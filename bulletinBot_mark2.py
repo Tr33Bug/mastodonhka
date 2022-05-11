d31eb25810726d6e1acd20fe14edeeb985ca8783
Tr33Bug <g-h-l-w@gmx.de>
2022-05-11 12:35:28 +0200

    change some stuff... WIP

--- bulletinBot_mark2.py
+++ bulletinBot_mark2.py
@@ -1,16 +1,38 @@
 from mastodon import Mastodon
-from config import MASTODON_TOKEN
+from config import MASTODON_TOKEN, LOGFILE
+from datetime import datetime, timedelta
+import requests
 
+LOGFILE += 'botLog.txt'
 
 mastodon = Mastodon(
     access_token = MASTODON_TOKEN,
     api_base_url = 'https://projekt-mastodon.h-ka-iwi.de/'
 )
-print(MASTODON_TOKEN)
+
+
+# hka-API credetials
+hkaUrl = 'https://www.iwi.h-ka.de/iwii/REST/newsbulletinboard/INFM'
+hkaHeaders = { 'accept': 'application/json' }
+
+
+# define Systemtime
+# systemTime = datetime.now() + timedelta(hours=TimeZoneDelta)
+
+# get request from hka API for news
+response = requests.get(hkaUrl, headers=hkaHeaders)
+news = response.json()
+print(news)
+
+logfile = open(LOGFILE, 'a')
+# logfile.write("Hello World mark 2!")
+logfile.close()
+    
+
 
 # print(mastodon.toot('Hello World mark2'))
 message = '@admin Hello World mark2'
-
+'''
 toot = Mastodon.status_post(self=mastodon, 
     status=message, 
     in_reply_to_id=None, 
@@ -27,3 +49,4 @@ toot = Mastodon.status_post(self=mastodon,
 print(toot)
 id = toot['id']
 print(id)
+'''
